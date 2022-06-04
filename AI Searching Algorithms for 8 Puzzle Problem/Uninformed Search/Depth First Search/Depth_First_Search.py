7cc523b055f0cff3a855a67bae9416771af55f91
ZeeshanaliZM <97593916+ZeeshanaliZM@users.noreply.github.com>
2022-06-04 19:31:03 +0530

    Update Depth_First_Search.py

--- AI Searching Algorithms for 8 Puzzle Problem/Uninformed Search/Depth First Search/Depth_First_Search.py
+++ AI Searching Algorithms for 8 Puzzle Problem/Uninformed Search/Depth First Search/Depth_First_Search.py
@@ -1,3 +1,7 @@
+#Here is the algorithm for Depth First Search.
+#The algorithm is most efficient when the goal is located really far from the intial state.
+#However because the searching used is list search, its time increases as more elements are added to the list.
+#Hence a better concept to search in lists like hash tables can be used.
 #Algorithm to search for a solution to 8 puzzle problem using Breadth First Search
 import numpy as np
 from copy import deepcopy
